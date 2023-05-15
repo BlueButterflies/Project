04ae533cd985432216fad94ea6add5d1b39424af
Butterfly Tech Creative Solution <52591976+BlueButterflies@users.noreply.github.com>
2023-05-15 14:40:50 +0200

    add new project for bar

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const full_name = document.getElementById('name');
+const email = document.getElementById('email');
+const select = document.getElementById('select');
+const data = document.getElementById('data');
+const hours = document.getElementById('hours');
+
+
+function ResetFormOrder(){
+    full_name.value = '';
+    email.value = '';
+    data.value = '';
+    hours.value = '';
+}
